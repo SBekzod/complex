3a83b88c367a81f4a94ba03340c6782b93d6ef69
Bekzo <bekzodsanakulov89@gmail.com>
2020-08-24 16:59:47 +0900

    testing router files on node.js

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const http = require('http')
 const express = require('express')
+const commute = require('./commute')
 
 //---------------
 const myapp = express()
 myapp.use(express.static('public'))
 myapp.set('views', 'views')
 myapp.set('view engine', 'ejs')
+console.log(commute.makeSound())
+
 
 myapp.get('/', function(req, res) {
     res.render('home-guest')
---
+++ commute.js
@@ -0,0 +1,6 @@
+console.log('passed through commute.js file')
+
+module.exports = { 
+    state: "unstaged", 
+    makeSound() {return "here is the unstaged sound"}
+}
