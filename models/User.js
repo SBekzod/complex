aa3af95ea309ee1f0bf86585bc299caa8aaac4a2
Bekzo <bekzodsanakulov89@gmail.com>
2020-08-30 23:38:54 +0900

    MODELS: prototype function classes

--- models/User.js
+++ models/User.js
@@ -1,29 +1,12 @@
 
-class User {
-    constructor(data){
-        this.data = data
-        this.planet = 'EARTH'
-    }
-
-    get planetName() {
-        return this.planet
-    }
-
-    set planetNameSet(a) {
-        this.planet = a
-    }
-
+// constructor
+let User = function (data) {
+    this.data = data
+    this.planet = 'Earth'
 }
 
+User.prototype.register = function () {
 
-// // constructor
-// let User = function (data) {
-//     this.data = data
-//     this.planet = 'Earth'
-// }
-
-// User.prototype.register = function () {
-
-// }
+}
 
 module.exports = User
