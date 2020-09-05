8f6cf89905b9a7efbf8effb78ee83ea5565b045a
Bekzo <bekzodsanakulov89@gmail.com>
2020-09-06 03:10:20 +0900

    Getting props in ejs using <% %>

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,28 +1,29 @@
-const User = require('../models/User')
+    const User = require('../models/User')
 
-////----------------------------------
+    ////----------------------------------
 
-exports.login = function (req, res) {
-}
-
-exports.logout = function (req, res) {
-}
-
-exports.home = function (req, res) {
-    res.render('home-guest')
-}
-
-exports.about = function (req, res) {
-    res.send('This page is about us')
-}
-
-exports.register = function (req, res) {
-    let user = new User(req.body)
-    user.register()
-    if(user.error.length) {
-        res.send(user.error)
-    } else {
-        res.send("No errors")
+    exports.login = function (req, res) {
     }
-   
-}
+
+    exports.logout = function (req, res) {
+    }
+
+    exports.home = function (req, res) {
+        // res.render('index', )
+        res.render('home-guest', {username: 'Damir', surname: 'Sanakulov'} )
+    }
+
+    exports.about = function (req, res) {
+        res.send('This page is about us')
+    }
+
+    exports.register = function (req, res) {
+        let user = new User(req.body)
+        user.register()
+        if(user.error.length) {
+            res.send(user.error)
+        } else {
+            res.send("No errors")
+        }
+    
+    }
