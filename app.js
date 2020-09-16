3d8fe5e53ec73da0ce66dc1d04269fcf3b90c60f
Bekzo <bekzodsanakulov89@gmail.com>
2020-09-16 18:42:51 +0900

    showing once login result based on sessions :npm: connect-flash

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express')
 const firstRouter = require('./router')
 const session = require('express-session')
 const MongoStore = require('connect-mongo')(session)
+const flash = require('connect-flash')
 
 //---------------
 const myapp = express()
@@ -13,6 +14,7 @@ let sessionOpt = session({
     saveUninitialized: false,
     cookie: { maxAge: 1000 * 60 * 2, httpOnly: true }
 })
+myapp.use(flash())
 
 myapp.use(sessionOpt)
 myapp.use(express.urlencoded({ extended: true }))
--- controllers/userController.js
+++ controllers/userController.js
@@ -13,7 +13,11 @@ userController.login = async function (req, res) {
             res.redirect('/')
         })
     } catch (err) {
-        res.send(err)
+        req.flash('errors', err)
+        // req.session.flash.errors = [err]
+        req.session.save(function(){
+            res.redirect('/')
+        })
     }
 
     // console.log('Final')
@@ -30,7 +34,7 @@ userController.home = function (req, res) {
         // console.log(req.session.user.username)
         res.render('home-dashboard', { username: req.session.user.username })
     } else {
-        res.render('home-guest', { username: '', surname: '' })
+        res.render('home-guest', { errors: req.flash('errors') })
     }
 
 }
