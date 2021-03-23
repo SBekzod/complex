539d468ca7c1403f57f56a291a1ed99e27217087
Martin <bekzodsanakulov89@gmail.com>
2021-03-23 18:20:17 +0900

    tempo

--- frontend-js/main.js
+++ frontend-js/main.js
@@ -1,17 +1,26 @@
 import Search from './modules/search'
 import Chat from './modules/chat'
+import Ltalk from './ltalk/ltalk'
 
 
 if (document.getElementById("009")) {
     let search = new Search()
 }
 
-if(document.querySelector("#chat-wrapper")) {
+if (document.querySelector("#chat-wrapper")) {
     new Chat()
 }
 
 
+if (document.querySelector('#ltalk')) {
+    let ltalk = new Ltalk()
+    ltalk.checking()
+
+
+}
+
+
 // Using JQuery here
-$(document).ready(function(){
+$(document).ready(function () {
 
 });
